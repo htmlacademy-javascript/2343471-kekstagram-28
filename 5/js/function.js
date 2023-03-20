b649182da62ed92bc43f806c5291920855fd922e
Keks <keks@htmlacademy.ru>
2023-03-20 10:51:02 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/function.js
+++ 5/js/function.js
@@ -12,12 +12,12 @@ return string.length <= length;
 */
 
 const isStringLess = (string, length) => {
-if (string.length <= length) {
-return true;
-} else {
-return false;
-}
-}
+  if (string.length <= length) {
+    return true;
+  } else {
+    return false;
+  }
+};
 isStringLess ('проверяемая строка', 20);
 
 // Строка является палиндромом
@@ -28,27 +28,29 @@ isStringLess ('проверяемая строка', 20);
 //имяФункции('Кекс');  // false
 
 const isPalindrom = (string) => {
-const tempString = string.toLowercase();
-let reverseString = '';
-for (let i = tempString.length - 1; i >= 0; i--) {
-reverseString += tempString.at(i);
-}
-console.log (reverseString);
-return tempString === reverseString;
-}
-const result= isPalindrom('');
+  const tempString = string.toLowerCase ();
+  let reverseString = '';
+  for (let i = tempString.length - 1; i >= 0; i--) {
+    reverseString += tempString.at(i);
+  }
+  //console.log (reverseString);
+  return tempString === reverseString;
+};
+isPalindrom ('');
 
 // Это палиндром
 //имяФункции('Лёша на полке клопа нашёл '); // true
 
 const extractNumber = (string) => {
-let result = '';
-for (let i = 0; i < spring.length; i++) {
-  if (!Number.isNaN (parseInt (string.at(i), 10))) {
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      result += string.at(i);
+    }
   }
-}
-return parseInt (result, 10);
-}
+  return parseInt (result, 10);
+};
+extractNumber ('2023 год');
 
 //имяФункции('2023 год');            // 2023
 //имяФункции('ECMAScript 2022');     // 2022
@@ -57,14 +59,15 @@ return parseInt (result, 10);
 //имяФункции('а я томат');           // NaN
 
 const myPadStart = (string, minLength, pad) => {
-let result = string;
-while (result.length < minLength) {
-const newResultLength = result.length + pad.length;
-const actualPad = newResultLength <=minLength ? pad : pad.slice(0, minLength - newResultLength);
-result = actualPad + result;
-}
-return result;
-}
+  let result = string;
+  while (result.length < minLength) {
+    const newResultLength = result.length + pad.length;
+    const actualPad = newResultLength <= minLength ? pad : pad.slice (0, minLength - newResultLength);
+    result = actualPad + result;
+  }
+  return result;
+};
+myPadStart ('q', 4, 'we');
 
 /*
 Добавочный символ использован один раз
